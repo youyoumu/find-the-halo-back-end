b3c31dc21aa15314cdec0d4b324c6ecc2a97ccef
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-05 23:03:33 +0700

    Respond game#new with image id

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,9 +1,10 @@
 class GameController < ApplicationController
-  def create
+  def new
     @game = Game.new
     @game.save
     hit_box = HitBox.find 1
     @game.hit_boxes << hit_box
-    render json: @game.hit_boxes
+
+    render json: 0
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", :as => :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "game#new"
 
   get "scores" => "scores#index"
   post "game" => "game#create"
