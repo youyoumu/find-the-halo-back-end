04dfa1f0dd0e96de9aabc01cf8f8838d0df6aa9f
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-06 00:05:40 +0700

    Remember user game id

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -4,7 +4,16 @@ class GameController < ApplicationController
     @game.save
     hit_box = HitBox.find 1
     @game.hit_boxes << hit_box
+    data = {
+      game_id: @game.id,
+      image_id: 0
+    }
 
-    render json: 0
+    render json: data
+  end
+
+  def create
+    puts "params :"
+    puts params
   end
 end
