60b516e8953d7732c6993b305c812badf1ab8735
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-06 11:16:41 +0700

    Send data when hit/miss

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -17,10 +17,19 @@ class GameController < ApplicationController
     @game = Game.find params[:game_id]
     @hit_box = @game.hit_boxes.where(image_id: params[:image_id]).first
     puts @hit_box.attributes
+    data = {
+      hit: false
+    }
     if params[:x] > @hit_box.x_start && params[:x] < @hit_box.x_end && params[:y] > @hit_box.y_start && params[:y] < @hit_box.y_end
       puts "hit"
+      @game.game_hit_boxes.where(hit_box_id: @hit_box.id).first.update(cleared: true)
+      data[:hit] = true
+      data[:coordinates] = {x_start: @hit_box.x_start, x_end: @hit_box.x_end, y_start: @hit_box.y_start, y_end: @hit_box.y_end}
     else
       puts "miss"
     end
+    puts @game.game_hit_boxes.where(hit_box_id: @hit_box.id).first.attributes
+
+    render json: data
   end
 end
