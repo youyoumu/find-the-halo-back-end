e1d8c7f2d069e3919d0da98b810e2ceb44c31414
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-05-06 17:08:09 +0700

    Fix query order

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -19,17 +19,18 @@ class GameController < ApplicationController
     @hit_box = @game.hit_boxes.where(image_id: params[:image_id], id: params[:hit_box_id]).first
     puts @hit_box.attributes
     total_hit_boxes = @game.game_hit_boxes.count
-    total_cleared = @game.game_hit_boxes.where(cleared: true).count
     data = {
       hit: false,
       total_hit_boxes: total_hit_boxes,
-      total_cleared: total_cleared
+      total_cleared: @game.game_hit_boxes.where(cleared: true).count
     }
     if params[:x] > @hit_box.x_start && params[:x] < @hit_box.x_end && params[:y] > @hit_box.y_start && params[:y] < @hit_box.y_end
       puts "hit"
       @game.game_hit_boxes.where(hit_box_id: @hit_box.id).first.update(cleared: true)
       data[:hit] = true
       data[:coordinates] = {x_start: @hit_box.x_start, x_end: @hit_box.x_end, y_start: @hit_box.y_start, y_end: @hit_box.y_end}
+      data[:total_cleared] = @game.game_hit_boxes.where(cleared: true).count
+
     else
       puts "miss"
     end
